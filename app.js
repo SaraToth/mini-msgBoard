da2ac37e99dd616170d1463700d2df8a473fc7eb
SaraToth <saratoth101@gmail.com>
2025-06-17 19:21:10 +0900

    Fix: app.get to app.use

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
+const path = require("node:path");
+const indexRouter = require("./routes/indexRouter");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+app.use("/", indexRouter);
 
 app.use((err, req, res, next) => {
     console.error(err);
