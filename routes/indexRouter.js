79e5b5e3ef91b1f75f3cf0f830b32fa901dd6dae
SaraToth <saratoth101@gmail.com>
2025-06-18 16:57:40 +0900

    Feat: Create a route for each message with id

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,13 +1,16 @@
 const { Router } = require("express");
 const indexRouter = Router();
+let idCount = 3;
 
 const messages = [
-    {
+    {   
+        id: 1,
         text: "Hi there!",
         user: "Amando",
         added: new Date(),
     },
     {
+        id: 2,
         text: "Hello World",
         user: "Charles",
         added: new Date(),
@@ -22,14 +25,23 @@ indexRouter.post("/new", (req, res) => {
     const { firstName, lastName, messageText } = req.body;
 
     const newMessage = {
+        id: idCount,
         user: firstName + " " + lastName,
         text: messageText,
         added: new Date(),
     };
 
     messages.push(newMessage);
-
+    idCount++;
     res.redirect("/");
+});
+
+
+indexRouter.get("/message/:msgId", (req, res) => {
+    const { msgId } = req.params;
+    const message = messages.find(mes => mes.id === Number(msgId));
+
+    res.render("message", { message: message});
 });
 
 indexRouter.get("/", (req, res) => {
