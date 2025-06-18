2ed5cee00efe34b34d8edcb4f5fed1994ead3c68
SaraToth <saratoth101@gmail.com>
2025-06-18 17:19:12 +0900

    Style: Simplify code

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -3,12 +3,8 @@ const indexRouter = Router();
 const { getForm, getIndex, postForm, getMessage } = require("../controllers/indexController");
 
 indexRouter.get("/new", getForm);
-
 indexRouter.post("/new", postForm);
-
-
 indexRouter.get("/message/:msgId", getMessage);
-
 indexRouter.get("/", getIndex);
 
 module.exports = indexRouter;
