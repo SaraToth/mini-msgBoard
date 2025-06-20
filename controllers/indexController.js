fd03065fdda1d1dc7b87428a942f1aa5183e7650
SaraToth <saratoth101@gmail.com>
2025-06-20 17:16:08 +0900

    Feat: Validate form inputs and add errors to views

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,4 +1,25 @@
 const db = require("../db/queries");
+const { body, validationResult } = require("express-validator");
+
+const alphaErr = "must only contain letters";
+const nameLengthErr = "must be between 1 and 35 characters";
+const emptyErr = "cannot be empty"
+
+const validateNewMessage = [
+    body("firstName").trim()
+        .notEmpty().withMessage(`First name ${emptyErr}`)
+        .isAlpha().withMessage(`First name ${alphaErr}`)
+        .isLength({ min: 1, max: 35}).withMessage(`First name ${nameLengthErr}`),
+
+    body("lastName").trim()
+        .notEmpty().withMessage(`Last name ${emptyErr}`)
+        .isAlpha().withMessage(`Last name ${alphaErr}`)
+        .isLength({ min: 1, max: 35}).withMessage(`Last name ${nameLengthErr}`),
+
+    body("messageText").trim()
+        .notEmpty().withMessage(`Message ${emptyErr}`)
+        .isLength({ min: 1, max: 300 }).withMessage(`Message must be between 1 and 300 characters`)
+];
 
 //Render form view
 const getForm = (req, res) => {
@@ -6,14 +27,22 @@ const getForm = (req, res) => {
 };
 
 //Submit form on post
-const postForm = async (req, res) => {
-    const { firstName, lastName, messageText } = req.body;
+const postForm = [
+    validateNewMessage,
 
-    const name = firstName + " " + lastName;
-    const date = new Date();
-    await db.createNewMessage(messageText, name, date);
-    res.redirect("/");
-};
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).render("form", { errors: errors.array()});
+        }
+
+        const { firstName, lastName, messageText } = req.body;
+        const name = firstName + " " + lastName;
+        const date = new Date();
+        await db.createNewMessage(messageText, name, date);
+        res.redirect("/");
+    },
+];
 
 // Get message view
 const getMessage = async (req, res) => {
